bff0bd07d8cc8afc832bf9e5d536c92abfe16504
les <nanotrippolo@gmail.com>
2015-02-06 18:48:18 +0100

    dbservices

--- js/json.js
+++ js/json.js
@@ -98,4 +98,26 @@
         "calories":480.0,
         "fat":21.0
     }
+    var flickr_result = {
+      "url":"https://flickr.com/photos/20170235@N03/6265967428",
+      "id":"6265967428",
+      "dateAdded":null,
+      "description":null,
+      "title":"Pizza Story :D"
+    }
+
+    /*var rest_in_db_all = {[
+    {"id":1,"firstname":"Mirko","lastname":"Morandi","birthdate":695538000000,"measureHistory":[{"mid":1,"measureValue":"80","dateRegistered":1335326400000,"measureType":"weight"},
+    {"mid":2,"measureValue":"93","dateRegistered":1416805200000,"measureType":"weight"},{"mid":3,"measureValue":"185","dateRegistered":1387688400000,"measureType":"height"},
+    {"mid":205,"measureValue":"110","dateRegistered":1322370000000,"measureType":"weight"},
+    {"mid":255,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":306,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"}
+    ,{"mid":307,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":308,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":356,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":406,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":506,"measureValue":"95","dateRegistered":1421557200000,"measureType":"weight"}],"currentHealth":[
+    {"mid":406,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":3,"measureValue":"185","dateRegistered":1387688400000,"measureType":"height"}],"lastBMI":32.14024835646457,"oldBMI":32.14024835646457},
+    "id":2,"firstname":"Chuck","lastname":"Norris","birthdate":-788904000000,"measureHistory":[{"mid":4,"measureValue":"120","dateRegistered":1299214800000,"measureType":"bloodpressure"},{"mid":5,"measureValue":"80","dateRegistered":1416891600000,"measureType":"weight"},{"mid":80,"measureValue":"165","dateRegistered":1416891600000,"measureType":"height"}]]}
+
+    var rest_in_db_person_details = {"id":1,"firstname":"Mirko","lastname":"Morandi","birthdate":695538000000,"measureHistory":[{"mid":1,"measureValue":"80","dateRegistered":1335326400000,"measureType":"weight"},{"mid":2,"measureValue":"93","dateRegistered":1416805200000,"measureType":"weight"},{"mid":3,"measureValue":"185","dateRegistered":1387688400000,"measureType":"height"},{"mid":205,"measureValue":"110","dateRegistered":1322370000000,"measureType":"weight"},{"mid":255,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":306,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":307,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":308,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":356,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":406,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":506,"measureValue":"95","dateRegistered":1421557200000,"measureType":"weight"}],"currentHealth":[{"mid":406,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":3,"measureValue":"185","dateRegistered":1387688400000,"measureType":"height"}],"lastBMI":32.14024835646457,"oldBMI":32.14024835646457}
+
+    var rest_in_db_measure_list = [{"mid":1,"measureValue":"80","dateRegistered":1335326400000,"measureType":"weight"},{"mid":2,"measureValue":"93","dateRegistered":1416805200000,"measureType":"weight"},{"mid":205,"measureValue":"110","dateRegistered":1322370000000,"measureType":"weight"},{"mid":255,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":306,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":307,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":308,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":356,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":406,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"},{"mid":506,"measureValue":"95","dateRegistered":1421557200000,"measureType":"weight"}]
+    var rest_in_db_measure_details = {"mid":306,"measureValue":"110","dateRegistered":1417064400000,"measureType":"weight"}*/
+
 
